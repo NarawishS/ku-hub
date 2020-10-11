02806b95ae6b0ae48a3bc4de078bc4d487b08649
NarawishS <narawish.s@ku.th>
2020-10-11 16:42:29 +0700

    refactor

--- kuhub/views.py
+++ kuhub/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 
+
 # Create your views here
 
 def home(request):
-    return render(request, "kuhub_html/home.html")
+    return render(request, "kuhub/home.html")
