26fa72c505298dc9bbb232ece911821afb96a391
keyboard2543 <keyboard2543@gmail.com>
2020-11-21 03:45:31 +0700

    Forum can be delete in forum index page while user is superuser and Forum cannot be duplicated

--- kuhub/models/blog.py
+++ kuhub/models/blog.py
@@ -6,7 +6,7 @@ from ckeditor.fields import RichTextField
 
 
 class BlogForum(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
--- kuhub/urls.py
+++ kuhub/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.BlogHome.as_view(), name='blog-home'),
     path('blog/<int:pk>/', views.BlogView.as_view(), name='blog-detail'),
     path('forum', views.BlogForumIndexView.as_view(), name='blog-forum-index'),
+    path('forum/<int:pk>/delete', views.delete_blog_forum, name='blog-forum-delete'),
     path('forum/<int:pk>/', views.BlogForumView.as_view(), name='blog-forum'),
     path('blog/<int:pk>/create_comment', views.CreateCommentView.as_view(), name='create_comment'),
     path('blog/<int:pk>/blog_report', views.BlogReportView.as_view(), name='blog_report'),
