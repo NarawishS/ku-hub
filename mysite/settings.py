a6f05bf9bf784c90baef63c9136aa244d83ddbc0
Narawish Sangsiriwut <narawish.s@ku.th>
2020-11-29 21:38:41 +0700

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 from decouple import config
 from django.urls import reverse_lazy
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY',default="secret_def")
+SECRET_KEY = config('SECRET_KEY', default="secret_def")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
@@ -179,6 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -204,3 +206,5 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 CKEDITOR_ALLOW_NONIMAGE_FILES = False
+
+django_heroku.settings(locals())
