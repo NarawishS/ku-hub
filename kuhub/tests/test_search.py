9bae53dfd2fd9e69aaa42cb49c58d47860a9ba20
keyboard2543 <keyboard2543@gmail.com>
2020-12-17 00:36:26 +0700

    Add testcases for searching keyword

--- kuhub/tests/test_search.py
+++ kuhub/tests/test_search.py
@@ -20,42 +20,41 @@ class SearchViewTest(TestCase):
             title="shibirinasai!?",
             short_description="Lisa voice!?",
             author=self.user1,
-            tags=TaggableManager()
         )
-        self.blog1.tags.name = "nakadashi"
+        self.blog1.tags.add(("nakadashi"))
         self.blog1.save()
 
     def test_searching_with_blog_title(self) -> None:
         keyword = self.blog1.title
         response = self.client.get(reverse('kuhub:blog-search'), {'keyword': keyword})
-        self.assertContains(response, keyword)
+        self.assertContains(response, self.blog1.title)
 
     def test_searching_with_blog_description(self) -> None:
         keyword = self.blog1.short_description
         response = self.client.get(reverse('kuhub:blog-search'), {'keyword': keyword})
-        self.assertContains(response, keyword)
+        self.assertContains(response, self.blog1.title)
 
     def test_searching_with_username(self) -> None:
         keyword = self.user1.username
         response = self.client.get(reverse('kuhub:blog-search'), {'keyword': keyword})
-        self.assertContains(response, keyword)
+        self.assertContains(response, self.blog1.title)
 
     def test_searching_with_first_name(self) -> None:
         keyword = self.user1.first_name
         response = self.client.get(reverse('kuhub:blog-search'), {'keyword': keyword})
-        self.assertContains(response, keyword)
+        self.assertContains(response, self.blog1.title)
 
     def test_searching_with_last_name(self) -> None:
         keyword = self.user1.last_name
         response = self.client.get(reverse('kuhub:blog-search'), {'keyword': keyword})
-        self.assertContains(response, keyword)
+        self.assertContains(response, self.blog1.title)
 
     def test_searching_with_email(self) -> None:
         keyword = self.user1.email
         response = self.client.get(reverse('kuhub:blog-search'), {'keyword': keyword})
-        self.assertContains(response, keyword)
+        self.assertContains(response, self.blog1.title)
 
     def test_searching_with_a_tag_name(self) -> None:
-        keyword = self.blog1.tags.name
+        keyword = "nakadashi"
         response = self.client.get(reverse('kuhub:blog-search'), {'keyword': keyword})
-        self.assertContains(response, keyword)
+        self.assertContains(response, self.blog1.title)
